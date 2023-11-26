fba23505d959c330d15da8d5e2dcb4776cfa21b9
KathidB <Kathidbb@gmail.com>
2023-11-26 10:53:07 +0100

    stylization for front-end part of signup component

--- FE/src/components/auth/signup/SignUp.js
+++ FE/src/components/auth/signup/SignUp.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import validate from '../validation/SignUpValidation'
+import RaccoonLogin from './images/raccoon-login.webp'
+import './signup.css'
+
 function SignUp () {
+  const [errors, setErrors] = useState(null)
+
   const handleSubmit = async (userData, { resetForm }) => {
     try {
-      console.log(userData)
       // Wysyłanie danych do backendu
       const response = await fetch('/api/register', {
         method: 'POST',
@@ -28,9 +34,16 @@ function SignUp () {
             email: ''
           }
         })
+        setErrors([
+          'Rejestracja zakończona sukcesem. Możesz się teraz zalogować.'
+        ])
       } else {
-        // Obsługa błędów, np. wyświetlenie komunikatu użytkownikowi
-        console.error('Error sending data to server:', response.statusText)
+        // W przypadku błędu, pobierz informacje o błędzie z serwera
+        const errorData = await response.text()
+        console.error('Error sending data to server:', errorData)
+
+        // Ustaw błąd, który zostanie wyświetlony użytkownikowi
+        setErrors(errorData.message || 'Wystąpił błąd podczas rejestracji.')
       }
     } catch (error) {
       // Obsługa błędów, np. wyświetlenie komunikatu użytkownikowi
@@ -40,65 +53,126 @@ function SignUp () {
 
   return (
     <section className='app-wrap'>
-      <Formik
-        initialValues={{
-          username: '',
-          password: '',
-          confirmPassword: '',
-          email: ''
-        }}
-        validate={validate}
-        onSubmit={handleSubmit}
-      >
-        {formik => (
-          <Form
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center'
-            }}
-          >
-            <h2>Sign Up</h2>
+      <div className='signup'>
+        <h2 className='section-title'>
+          Sign <span className='span-brand'>Up</span>
+        </h2>
 
-            <label htmlFor='username'>Nickname</label>
-            <Field
-              type='text'
-              id='username'
-              name='username'
-              placeholder='Nickname'
-            />
-            <ErrorMessage name='username' component='div' />
+        <img
+          className='raccon-login-img'
+          src={RaccoonLogin}
+          alt=''
+          width={200}
+          height={200}
+        />
 
-            <label htmlFor='password'>Hasło</label>
-            <Field
-              type='password'
-              id='password'
-              name='password'
-              placeholder='********'
-            />
-            <ErrorMessage name='password' component='div' />
+        <Formik
+          initialValues={{
+            username: '',
+            password: '',
+            confirmPassword: '',
+            email: ''
+          }}
+          validate={validate}
+          onSubmit={handleSubmit}
+        >
+          {formik => (
+            <Form className='form-signup'>
+              <p>Stwórz konto i dołącz do społeczności!</p>
 
-            <label htmlFor='confirmPassword'>Potwierdź hasło:</label>
-            <Field
-              type='password'
-              id='confirmPassword'
-              name='confirmPassword'
-              placeholder='********'
-            />
-            <ErrorMessage name='confirmPassword' component='div' />
+              <label htmlFor='email'>Email</label>
+              <Field
+                type='email'
+                id='email'
+                name='email'
+                placeholder='Email'
+                className={
+                  formik.touched.email && formik.errors.email
+                    ? 'signup-input-error'
+                    : ''
+                }
+              />
+              <ErrorMessage
+                name='email'
+                component='span'
+                className='signup-error-msg'
+              />
 
-            <label htmlFor='email'>Email</label>
-            <Field type='email' id='email' name='email' placeholder='Email' />
-            <ErrorMessage name='email' component='div' />
+              <label htmlFor='username'>Nickname</label>
+              <Field
+                type='text'
+                id='username'
+                name='username'
+                placeholder='Nickname'
+                className={
+                  formik.touched.username && formik.errors.username
+                    ? 'signup-input-error'
+                    : ''
+                }
+              />
+              <ErrorMessage
+                name='username'
+                component='span'
+                className='signup-error-msg'
+              />
 
-            <button type='submit' disabled={!(formik.dirty && formik.isValid)}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+              <label htmlFor='password'>Hasło</label>
+              <Field
+                type='password'
+                id='password'
+                name='password'
+                placeholder='*********'
+                className={
+                  formik.touched.password && formik.errors.password
+                    ? 'signup-input-error'
+                    : ''
+                }
+              />
+              <ErrorMessage
+                name='password'
+                component='span'
+                className='signup-error-msg'
+              />
+
+              <label htmlFor='confirmPassword'>Potwierdź hasło</label>
+              <Field
+                type='password'
+                id='confirmPassword'
+                name='confirmPassword'
+                placeholder='*********'
+                className={
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                    ? 'signup-input-error'
+                    : ''
+                }
+              />
+              <ErrorMessage
+                name='confirmPassword'
+                component='span'
+                className='signup-error-msg'
+              />
+
+              <button
+                type='submit'
+                className='signup-submit-button'
+                disabled={!(formik.dirty && formik.isValid)}
+              >
+                Submit
+              </button>
+
+              {errors && <div className='signup-error-msg'>{errors}</div>}
+            </Form>
+          )}
+        </Formik>
+
+        <div className='form-to-login'>
+          Posiadasz już konto?{' '}
+          <NavLink to='/login' className='signup-login'>
+            Zaloguj się
+          </NavLink>
+        </div>
+      </div>
     </section>
   )
 }
--- FE/src/components/auth/validation/SignUpValidation.js
+++ FE/src/components/auth/validation/SignUpValidation.js
@@ -2,7 +2,7 @@ const validate = values => {
   const errors = {}
 
   if (!values.username) {
-    errors.username = 'Required'
+    errors.username = 'Pole wymagane'
   } else if (values.username.length < 3 || values.username.length >= 30) {
     errors.username = 'Nick musi mieć od 3 do 30 znaków'
   }
@@ -10,8 +10,7 @@ const validate = values => {
   if (values.password.length < 8) {
     errors.password = 'Hasło musi mieć co najmniej 8 znaków'
   } else if (!/(?=.*[!@#$%^&*])/.test(values.password)) {
-    errors.password =
-      'Hasło musi zawierać co najmniej jeden znak specjalny (!@#$%^&*)'
+    errors.password = `Hasło musi zawierać co najmniej jeden znak specjalny (!@#$%^&*)`
   }
 
   if (values.password !== values.confirmPassword) {
