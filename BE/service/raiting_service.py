c79848517d3cd9aa12345a0921f190669d07cb25
Micha Motyka <szortownikq@gmail.com>
2023-11-28 00:10:57 +0100

    unit tests rating

--- BE/service/raiting_service.py
+++ BE/service/raiting_service.py
@@ -1,5 +1,6 @@
 from entity.rating import Rating
 from shared.base import session_factory
+from sqlalchemy import update
 import uuid
 
 def create_raiting():
@@ -9,3 +10,22 @@ def create_raiting():
         session.commit()
         session.refresh(rating)
     return rating
+
+def update_raiting(id:int,isWin:bool):
+    with session_factory() as session:
+        rating:Rating = session.query(Rating).filter_by(id = id).first()
+        rating.bets += 1
+        if isWin:
+            rating.wins += 1
+        rate = calc_rating(rating=rating)
+        stmt = update(Rating).where(Rating.id == rating.id).values(rating = rate)
+        session.execute(stmt)
+        session.commit()
+
+
+
+def calc_rating(rating:Rating):
+    try:
+        return 100 * rating.wins / rating.bets
+    except ZeroDivisionError:
+        return 0
