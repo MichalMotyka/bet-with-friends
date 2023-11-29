27c702800e82fb2d8d5c0343f657e6e2e0e5f198
Micha Motyka <szortownikq@gmail.com>
2023-11-29 12:59:50 +0100

    avatar list

--- BE/controllers/image_controller.py
+++ BE/controllers/image_controller.py
@@ -1,10 +1,12 @@
 import io
 from flask import Blueprint, request, send_file, send_from_directory
+from flask_cors import CORS, cross_origin
 import os
 
 image_blueprint = Blueprint('image_blueprint', __name__)
 
 @image_blueprint.route('/image/<id>',methods=['GET'])
+@cross_origin()
 def download(id):
     directory = os.path.dirname(__file__)
     directory = os.path.abspath(os.path.join(directory, os.pardir))
@@ -13,6 +15,7 @@ def download(id):
     return send_file(file_path, as_attachment=True) 
 
 @image_blueprint.route('/avatar/<id>',methods=['GET'])
+@cross_origin()
 def avatar(id):
     directory = os.path.join(os.path.dirname(__file__), os.pardir, 'resources', 'avatars')
     file_path = os.path.join(directory, f"{id}.webp")
@@ -24,3 +27,12 @@ def avatar(id):
         io.BytesIO(image),
         mimetype='image/webp'
     )
+
+@image_blueprint.route('/avatar',methods=['GET'])
+@cross_origin()
+def avatars():
+    directory = os.path.join(os.path.dirname(__file__), os.pardir, 'resources', 'avatars')
+    file_arr = os.listdir(directory)
+    for index,file in enumerate(file_arr):
+        file_arr[index] = {'avatar':file.split('.')[0]}
+    return file_arr
