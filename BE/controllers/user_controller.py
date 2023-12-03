0cbee6014161b08da6850ab1d6ce310d2f9efe33
Micha Motyka <szortownikq@gmail.com>
2023-12-03 19:57:29 +0100

    fix logout

--- BE/controllers/user_controller.py
+++ BE/controllers/user_controller.py
@@ -49,9 +49,9 @@ def login():
             authorize, refresh = jwtTokens
             response = make_response(Response(message='The user has been successfully logged in.',code='OK').__dict__)
             expiration = datetime.utcnow() + timedelta(minutes=int(config.get_config_by_key("jwt.exp.authorization")))
-            response.set_cookie('Authorization',authorize,expires=expiration,httponly=False,samesite=None,max_age=expiration,domain=request.origin)
+            response.set_cookie('Authorization',authorize,expires=expiration.timestamp(),httponly=False)
             expiration = datetime.utcnow() + timedelta(minutes=int(config.get_config_by_key("jwt.exp.refresh")))
-            response.set_cookie('Refresh',refresh,expires=expiration,httponly=False,samesite=None,max_age=expiration,domain=request.origin)
+            response.set_cookie('Refresh',refresh,expires=expiration.timestamp(),httponly=False)
     except (PasswordOrLoginIncorrectException, UserNotActivatedException) as e:
          response = make_response(Response(message=e.message,code=e.code).__dict__)
          response.status_code = 400
@@ -63,7 +63,7 @@ def login():
 @user_blueprint.route('/logout', methods=['GET'])
 def logout():
     response = make_response(Response(message='The user has been successfully logout.',code='OK').__dict__)
-    response.set_cookie('Authorization','',expires=0,httponly=True,samesite=None,max_age=0,domain=request.origin)
-    response.set_cookie('Refresh','',expires=0,httponly=True,samesite=None,max_age=0,domain=request.origin)
+    response.set_cookie('Authorization','',1,httponly=False)
+    response.set_cookie('Refresh','',expires=1,httponly=False)
     return response
 
