4355a87194ab179434ad0bf2ffd3682711d01242
Michał Motyka <86515008+MichalMotyka@users.noreply.github.com>
2023-12-11 23:16:31 +0100

    Revert "Dev"

--- FE/src/components/auth/login/Login.js
+++ FE/src/components/auth/login/Login.js
@@ -5,6 +5,7 @@ import validate from '../validation/LogInValidations'
 import RaccoonLogin from './images/raccoon-login3.webp'
 import { ScrollToTop } from '../../utilities/ScrollToTop'
 import { FaSpinner } from 'react-icons/fa'
+
 import { useAuth } from '../authcontext/AuthContext'
 
 import './login.css'
