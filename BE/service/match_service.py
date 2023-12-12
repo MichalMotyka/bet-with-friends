e4c4dc53d3594511a36ef8a9b1c32a8a561852b4
Micha Motyka <szortownikq@gmail.com>
2023-12-12 17:10:11 +0100

    bets fi

--- BE/service/match_service.py
+++ BE/service/match_service.py
@@ -92,32 +92,27 @@ def get_competetition_list():
 def get_posible_bets(competetition,page:int,limit:int,user:Users) -> [Match]:
     with session_factory() as session:
         profile = session.query(Profile).filter(Profile.user_id == user.id).first()
-        matches = session.query(Bets.match_id).filter(Bets.profile_id == profile.id)
         possible_best = (session
             .query(Match)
             .join(Competition, Match.competetition_id == Competition.id)
-            .outerjoin(Bets, Match.id == Bets.match_id)
             .filter(
                 Competition.public_id == competetition,
                 Match.utc_date >= datetime.utcnow(),
                 Match.utc_date <= datetime.now() + timedelta(days=5),
-                Bets.match_id.not_in(matches)
             )
             .order_by(Match.utc_date)
             .offset((page - 1) * limit)
             .limit(limit)
             .all()
         )
-        count = (session
-            .query(Match)
-            .join(Competition, Match.competetition_id == Competition.id)
-            .outerjoin(Bets, Match.id == Bets.match_id)
-            .filter(
-                Competition.public_id == competetition,
-                Match.utc_date >= datetime.utcnow(),
-                Match.utc_date <= datetime.now() + timedelta(days=5)
-            ).count()
-        )
+
+        result:[Match] = []
+        for match in possible_best:
+            bet = session.query(Bets).filter(Bets.profile_id == profile.id,Bets.match_id == match.id).first()
+            if not bet:
+                result.append(match)
+
+        count =  len(result)
     return (possible_best, count)
 
 def proces_bets():
