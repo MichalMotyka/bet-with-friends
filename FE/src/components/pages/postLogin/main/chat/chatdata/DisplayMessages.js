05576a010e1f53c7ccb77883eba5b5c9e5bcdca4
KathidB <Kathidbb@gmail.com>
2023-12-17 11:17:45 +0100

    chat get message done

--- FE/src/components/pages/postLogin/main/chat/chatdata/DisplayMessages.js
+++ FE/src/components/pages/postLogin/main/chat/chatdata/DisplayMessages.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery, gql } from '@apollo/client'
+import { useRef, useEffect } from 'react'
 
 const GET_MESSAGES = gql`
   query MyQuery {
@@ -16,32 +17,57 @@ const GET_MESSAGES = gql`
 `
 
 function DisplayMessages () {
+  const [userMsg, setUserMsg] = useState(null)
+  const messagesContainerRef = useRef(null)
   const { loading, error, data } = useQuery(GET_MESSAGES, {
     variables: { limit: 10, page: 1 }
   })
+
+  useEffect(() => {
+    // Przewiń okno w dół po pierwszym renderowaniu
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight
+    }
+  }, [data]) // Użyj data jako zależności, aby upewnić się, że useEffect wykonuje się po otrzymaniu danych
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>
 
   return (
-    <div className='chat'>
-      {data.getMessages
-        .map(message => (
-          <div key={message.uuid} className='chat-box'>
-            <img
-              src={message.sender.avatar}
-              height={35}
-              alt={`${message.sender.name}'s avatar`}
-            />
-            <p> {message.sender.ranking}</p>
-            <p>{message.sender.name}</p>
-            <div className='chat-box-msg'>
-              <p> {message.content}</p>
-            </div>
-          </div>
-        ))
-        .reverse()}
-    </div>
+    <>
+      <div className='chat' ref={messagesContainerRef}>
+        {data.getMessages
+          .map(message => (
+            <ul className='chat-box'>
+              <li key={message.uuid} className='chat-box-item'>
+                <img
+                  className='chat-avatar'
+                  src={message.sender.avatar}
+                  height={30}
+                  alt={`${message.sender.name}'s avatar`}
+                />
+                <p> {message.sender.ranking}</p>
+                <p>{message.sender.name}:</p>
+                <div className='chat-box-msg'>
+                  <p> {message.content}</p>
+                </div>
+              </li>
+            </ul>
+          ))
+          .reverse()}
+        <div className='chat-input-box'>
+          <input
+            value={userMsg}
+            className='chat-input'
+            type='text'
+            placeholder='Wiadomość...'
+            onChange={e => setUserMsg(e.target.value)}
+          />
+          <button>Wyślij</button>
+        </div>
+      </div>
+    </>
   )
 }
 
