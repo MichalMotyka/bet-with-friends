ee41f541ab559a8a5e77a5929f3fff54c5e8b670
Micha Motyka <szortownikq@gmail.com>
2023-12-22 16:19:38 +0100

    add ddl

--- BE/service/achivment_service.py
+++ BE/service/achivment_service.py
@@ -1,5 +1,8 @@
 from service.achivment_repository import get_list_of_achiv
+from sqlalchemy import func
 from shared.base import session_factory
+from entity.achivments import Achivments
+from entity.profile import Profile
 
 
 def insert_achivments(profile_id):
@@ -7,3 +10,13 @@ def insert_achivments(profile_id):
         for achiv in get_list_of_achiv(profile_id=profile_id):
             session.add(achiv)
             session.commit()
+
+def update_achivments_for_old_users():
+    with session_factory() as session:
+        profiles:[] = session.query(Achivments.profile_id).group_by(Achivments.profile_id).having(func.count() < 5).all()
+        profiles+=(session.query(Profile.id).outerjoin(Achivments).all())
+        for profile in profiles:
+            for achiv in get_list_of_achiv(profile_id=profile[0]):
+                if not session.query(Achivments).filter(Achivments.profile_id == profile[0], Achivments.achivment_type_uuid == achiv.achivment_type_uuid).first():
+                    session.add(achiv)
+                    session.commit()
