abaeb481318ff15fe4024707a2a8ed619f47f87d
Micha Motyka <szortownikq@gmail.com>
2023-12-30 18:59:03 +0100

    update dll

--- BE/resources/ddl/triggers_create.sql
+++ BE/resources/ddl/triggers_create.sql
@@ -17,6 +17,7 @@ BEGIN
         EXECUTE text_value INTO execution_result;
         IF execution_result THEN
             UPDATE achievement SET active = true,acquired=now() WHERE profile_id = new.profile_id AND id = id_value;
+            UPDATE profiles SET experience = experience + 100 where id = new.profile_id;
         END IF;
     END LOOP;
     RETURN NULL;
@@ -24,7 +25,6 @@ END;
 $$;
 
 alter function process_achievement_ratings() owner to postgres;
-
 
 create function process_achievement() returns trigger
     language plpgsql
@@ -43,6 +43,7 @@ BEGIN
         EXECUTE text_value INTO execution_result;
         IF execution_result THEN
             UPDATE achievement SET active = true,acquired=now() WHERE profile_id = new.profile_id AND id = id_value;
+            UPDATE profiles SET experience = experience + 100 where id = new.profile_id;
         END IF;
     END LOOP;
     RETURN NULL;
@@ -50,6 +51,23 @@ END;
 $$;
 
 alter function process_achievement() owner to postgres;
+
+create function process_experience() returns trigger
+    language plpgsql
+as
+$$
+DECLARE
+    exp BOOLEAN;
+BEGIN
+    Select experience from profiles where id = new.id into exp;
+    if exp >= 100 then
+        UPDATE profiles SET experience = exp - 100 and level = level +1 where id = new.id;
+    end if;
+    RETURN NULL;
+END;
+$$;
+
+alter function process_experience() owner to postgres;
 
 
 create trigger process_achievement_trigger
@@ -70,3 +88,9 @@ create trigger process_achievement_trigger
     on ratings
     for each row
 execute procedure process_achievement_ratings();
+
+create trigger process_experience_trigger
+    after update
+    on profiles
+    for each row
+execute procedure process_experience();
