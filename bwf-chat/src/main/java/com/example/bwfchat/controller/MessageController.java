b2a6ed75f147c1908679f204d7ba820a8fb393b4
Micha Motyka <szortownikq@gmail.com>
2024-01-02 19:30:08 +0100

    set read status

--- bwf-chat/src/main/java/com/example/bwfchat/controller/MessageController.java
+++ bwf-chat/src/main/java/com/example/bwfchat/controller/MessageController.java
@@ -7,6 +7,7 @@ import com.example.bwfchat.entity.SystemInfo;
 import com.example.bwfchat.events.DatabaseChangeService;
 import com.example.bwfchat.events.DatabaseChangeServiceSystemInfo;
 import com.example.bwfchat.mediator.MessageMediator;
+import com.example.bwfchat.mediator.SystemInfoMediator;
 import com.example.bwfchat.services.JwtService;
 import com.example.bwfchat.services.SystemInfoService;
 import jakarta.servlet.http.Cookie;
@@ -32,7 +33,7 @@ import java.util.List;
 public class MessageController {
 
     private final MessageMediator messageMediator;
-    private final SystemInfoService systemInfoService;
+    private final SystemInfoMediator systemInfoMediator;
     private final DatabaseChangeService databaseChangeService;
     private final DatabaseChangeServiceSystemInfo databaseChangeServiceSystemInfo;
 
@@ -53,8 +54,8 @@ public class MessageController {
         Cookie[] cookies = httpServletRequest.getCookies();
         HttpServletResponse httpServletResponse = attributes.getResponse();
         String userID = messageMediator.validateCookies(cookies);
-        httpServletResponse.addHeader("X-Total-Count", String.valueOf(systemInfoService.getTotalCount(userID)));
-        return systemInfoService.getSystemInfo(userID,page-1, limit);
+        httpServletResponse.addHeader("X-Total-Count", String.valueOf(systemInfoMediator.getTotalCount(userID)));
+        return systemInfoMediator.getSystemInfo(page-1, limit,userID);
     }
 
     @MutationMapping
@@ -65,6 +66,16 @@ public class MessageController {
         messageMediator.sendMessage(message,cookies);
 
         return new Response("Message has been send","OK");
+    }
+
+    @MutationMapping
+    public Response readSystemInfo(@Argument String uuid){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest httpServletRequest = attributes.getRequest();
+        Cookie[] cookies = httpServletRequest.getCookies();
+        systemInfoMediator.readSystemInfo(uuid,cookies);
+
+        return new Response("Message has been read","OK");
     }
 
     @MutationMapping
--- bwf-chat/src/main/java/com/example/bwfchat/repository/SystemInfoRepository.java
+++ bwf-chat/src/main/java/com/example/bwfchat/repository/SystemInfoRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SystemInfoRepository extends JpaRepository<SystemInfo,Long> {
 
     @Query(nativeQuery = true,value = "SELECT * from msgs.system_info ORDER BY msgs.system_info.id DESC OFFSET :page limit :limit")
     List<SystemInfo> findAll(@Param("page") int page, @Param("limit") int limit);
     long countSystemInfoByProfileId(long profileId);
+
+    SystemInfo findByUuid(String uuid);
 }
