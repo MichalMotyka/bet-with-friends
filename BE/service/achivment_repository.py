dcc6500f7498e3016cca3c061521cf67ff70fb18
Micha Motyka <szortownikq@gmail.com>
2024-01-06 17:35:22 +0100

    more achivments

--- BE/service/achivment_repository.py
+++ BE/service/achivment_repository.py
@@ -11,9 +11,19 @@ def get_list_of_achiv(profile_id:int):
             generate_25_bet(profile_id),
             generate_50_bet(profile_id),
             generate_100_bet(profile_id),
+            generate_250_bet(profile_id),
+            generate_500_bet(profile_id),
             generate_1000_bet(profile_id),
+            generate_1500_bet(profile_id),
+            generate_2000_bet(profile_id),
             generate_25_ratings(profile_id),
-            generate_50_ratings(profile_id)]
+            generate_50_ratings(profile_id),
+            generate_5_bets_correct(profile_id),
+            generate_25_bets_correct(profile_id),
+            generate_100_bets_correct(profile_id),
+            generate_250_bets_correct(profile_id),
+            generate_500_bets_correct(profile_id),
+            generate_1000_bets_correct(profile_id)]
 
 
 def generate_przegryw(profile_id:int) -> Achivments:
@@ -161,6 +171,26 @@ def generate_100_bet(profile_id:int) -> Achivments:
         image_url='',
         query_text=f"""SELECT count(id) > 100 as result from bets where profile_id = {profile_id}""")
 
+def generate_250_bet(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='e63c0991-1dc6-4efb-8c6c-b4b00c6e3df8',
+        achiv_name='Poprostu 250',
+        profile_id=profile_id,
+        description='Utwórz 250 betów',
+        image_url='',
+        query_text=f"""SELECT count(id) > 250 as result from bets where profile_id = {profile_id}""")
+
+def generate_500_bet(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='8319de45-20a5-4f40-b60e-c2e96a1aaeda',
+        achiv_name='Więcej betów',
+        profile_id=profile_id,
+        description='Utwórz 500 betów',
+        image_url='',
+        query_text=f"""SELECT count(id) > 500 as result from bets where profile_id = {profile_id}""")
+
 def generate_1000_bet(profile_id:int) -> Achivments:
     return Achivments(
         uuid=uuid.uuid4(),
@@ -170,6 +200,26 @@ def generate_1000_bet(profile_id:int) -> Achivments:
         description='Utwórz 1000 betów',
         image_url='',
         query_text=f"""SELECT count(id) > 1000 as result from bets where profile_id = {profile_id}""")
+
+def generate_1500_bet(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='69cc046d-633f-4de9-a49e-464b7b8d740a',
+        achiv_name='1500 kliknięć',
+        profile_id=profile_id,
+        description='Utwórz 1500 betów',
+        image_url='',
+        query_text=f"""SELECT count(id) > 1500 as result from bets where profile_id = {profile_id}""")
+
+def generate_2000_bet(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='f7d37a1a-286b-4fd7-814d-8fa0ef2c4444',
+        achiv_name='Drugi tysiąc trzeba ukraść',
+        profile_id=profile_id,
+        description='Utwórz 2000 betów',
+        image_url='',
+        query_text=f"""SELECT count(id) > 2000 as result from bets where profile_id = {profile_id}""")
 
 def generate_25_ratings(profile_id:int) -> Achivments:
     return Achivments(
@@ -184,9 +234,70 @@ def generate_25_ratings(profile_id:int) -> Achivments:
 def generate_50_ratings(profile_id:int) -> Achivments:
     return Achivments(
         uuid=uuid.uuid4(),
-        achivment_type_uuid='d87bb683-37d1-4295-b18d-4c15a37ac501',
+        achivment_type_uuid='01190eea-4998-48dc-8c4d-99e1dd9f552c',
         achiv_name='50/50',
         profile_id=profile_id,
         description='Osiągnij 50% skuteczności betów',
         image_url='',
         query_text=f"""select r.rating >= 50 as result from profiles join ratings r on profiles.rating_id = r.id where profiles.id = {profile_id}""")
+
+
+def generate_5_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='955809cc-68b0-4fde-b0b8-0ceb849a390e',
+        achiv_name='Zwycięska 5',
+        profile_id=profile_id,
+        description='Obstaw 5 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 5 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
+
+def generate_25_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='e7bdc55c-0aa7-47b1-a7ad-fccfd33589e0',
+        achiv_name='5x5',
+        profile_id=profile_id,
+        description='Obstaw 25 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 25 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
+
+def generate_100_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='141e4aba-54d0-410e-b059-65f9f9e6c20f',
+        achiv_name='To nie przypadek',
+        profile_id=profile_id,
+        description='Obstaw 100 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 100 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
+
+def generate_250_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='62261fde-2bc2-46ac-af56-c82a824b066f',
+        achiv_name='Kozak we wsi',
+        profile_id=profile_id,
+        description='Obstaw 250 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 250 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
+
+def generate_500_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='e15b0a9a-2eef-40c2-9b0e-a0662eda63c3',
+        achiv_name='Koxu',
+        profile_id=profile_id,
+        description='Obstaw 500 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 500 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
+
+def generate_1000_bets_correct(profile_id:int) -> Achivments:
+    return Achivments(
+        uuid=uuid.uuid4(),
+        achivment_type_uuid='5cbf8038-9dc8-40a3-bb7c-fc062b0bcc8c',
+        achiv_name='Legenda',
+        profile_id=profile_id,
+        description='Obstaw 1000 meczy prawidłowo',
+        image_url='',
+        query_text=f"""select count(bets.id) >= 1000 from bets join match m on m.id = bets.match_id join score s on s.id = m.score_id where s.full_time = concat(bets.home_team,'-',bets.away_team) and bets.profile_id = {profile_id}""")
